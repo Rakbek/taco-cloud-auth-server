35d588fe4fbc568b38e0db0298ef8a490dee4d85
Michele delle Noci <michele.dellenoci@gmail.com>
2022-07-18 09:30:37 +0200

    init

---
+++ src/main/java/sia/tacocloudauthserver/TacoCloudAuthServerApplication.java
@@ -0,0 +1,13 @@
+package sia.tacocloudauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudAuthServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sia/tacocloudauthserver/TacoCloudAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package sia.tacocloudauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
